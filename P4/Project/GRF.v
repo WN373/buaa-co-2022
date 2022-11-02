5f34e2c22e5f95f5ebcabb17acf5330b59d953cc
Xjchen <wnico233@outlook.com>
2022-11-02 20:18:32 +0800

    starting P4 undercourse

---
+++ P4/Project/GRF.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:01:50 11/02/2022 
+// Design Name: 
+// Module Name:    GRF 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module GRF(
+    input clk,
+    input reset,
+    input WE,
+    input [4:0] A1,
+    input [4:0] A2,
+    input [4:0] A3,
+    input [31:0] WD,
+    output [31:0] RD1,
+    output [31:0] RD2
+    );
+	 
+	 
+	 
+
+
+endmodule
---
+++ P4/Project/mips.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:59:16 11/02/2022 
+// Design Name: 
+// Module Name:    mips 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mips(
+    input clk,
+    input reset
+    );
+
+
+endmodule
